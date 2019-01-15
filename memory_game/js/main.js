717b615cadf9cb2c08a7e8ccde2505dc2d8f0a9d
Ralphie Celedon <Ralphie@candaces-air.attlocal.net>
2019-01-14 22:21:02 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = [0];
+cardsInPlay.push(cards[0]);
+{
+console.log("User flipped " + cards[0]);
+};
+
+var cardTwo = [2];
+cardsInPlay.push(cards[2]);
+{
+	console.log("User flipped " + cards[2]);
+};
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again!");
+		}
+	}
