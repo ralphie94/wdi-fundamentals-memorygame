8d2d181ab7916f18fc49e0ea6880d12dec5dfd95
Ralphie Celedon <Ralphie@candaces-air.attlocal.net>
2019-01-16 20:47:41 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -34,9 +34,10 @@ var checkForMatch = function () {
 	}
 
 
-var flipCard = function (cardId) {
-	
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	
@@ -48,5 +49,14 @@ console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
